9c81b3199a59ce9fbba64d49a397b2b1f88be2b7
gen-how <billchen85@gmail.com>
2025-09-02 14:44:47 +0800

    feat: solve 0704

---
+++ leetcode/0704/0704.go
@@ -0,0 +1,19 @@
+package leetcode
+
+// https://leetcode.com/problems/binary-search
+func search(nums []int, target int) int {
+	// Searches for the interval of [l, r).
+	l, r := 0, len(nums)
+	for l < r {
+		// m := l + (r-l)/2
+		m := (l + r) / 2
+		if nums[m] > target {
+			r = m
+		} else if nums[m] < target {
+			l = m + 1
+		} else {
+			return m
+		}
+	}
+	return -1
+}
---
+++ leetcode/0704/0704_test.go
@@ -0,0 +1,39 @@
+package leetcode
+
+import "testing"
+
+func Test_search(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "Case 1",
+			args: args{nums: []int{-1, 0, 3, 5, 9, 12}, target: 9},
+			want: 4,
+		},
+		{
+			name: "Case 2",
+			args: args{nums: []int{-1, 0, 3, 5, 9, 12}, target: 2},
+			want: -1,
+		},
+		{
+			name: "Case 3",
+			args: args{nums: []int{0, 1, 2}, target: 2},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := search(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("search(%v, %v) = %v, want %v", tt.args.nums, tt.args.target, got, tt.want)
+			}
+		})
+	}
+}
